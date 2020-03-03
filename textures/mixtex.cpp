85a826ec65899172ac824fa06b0cb89cce9c18a3
sfzhang <shengfazhang@outlook.com>
2020-03-03 12:11:42 +0800

    add textures project

---
+++ textures/mixtex.cpp
@@ -0,0 +1,225 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Need VAO in core profile
+
+    // Create window
+    GLFWwindow * window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+    if (!window) {
+        cout << "glfwCreateWindow() failed!" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        cout << "gladLoadGLLoader() failed" << endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    float vertices[] = {
+        // pos               color               texture coordinate
+         0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   1.0f, 1.0f,
+         0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,   0.0f, 0.0f,
+        -0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 0.0f,   0.0f, 1.0f
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2,
+        0, 2, 3
+    };
+    
+    // VAO
+    unsigned int vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // VBO
+    unsigned int vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    // EBO
+    unsigned int ebo;
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // Texture
+    unsigned int tex[2];
+    for (int i = 0; i < 2; i++) {
+        glGenTextures(1, &tex[i]);
+        glActiveTexture(GL_TEXTURE0 + i);
+        glBindTexture(GL_TEXTURE_2D, tex[i]);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    }
+
+    // texture 0
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, tex[0]);
+
+    int width, height, channels;
+    unsigned char *data = stbi_load("../res/container.jpg", &width, &height, &channels, 0);
+    if (!data) {
+        cout << "stbi_load() failed" << endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(data);
+
+    // texture 1
+    glActiveTexture(GL_TEXTURE0 + 1);
+    glBindTexture(GL_TEXTURE_2D, tex[1]);
+
+    stbi_set_flip_vertically_on_load(true);
+    data = stbi_load("../res/awesomeface.png", &width, &height, &channels, 0);
+    if (!data) {
+        cout << "stbi_load() failed" << endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(data);
+
+    // Vertex Shader
+    const char *vs_glsl[] = {
+        "#version 460 core\n"
+        "layout (location = 0) in vec3 pos;\n"
+        "layout (location = 1) in vec3 color;\n"
+        "layout (location = 2) in vec2 tex_coord;\n"
+        "out vec4 vertex_color;\n"
+        "out vec2 vertex_tex_coord;\n"
+        "out vec2 vertex_tex_coord2;\n" // flip awesomeface
+        "void main() {\n"
+        "    gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);\n"
+        "    vertex_color = vec4(color, 1.0f);\n"
+        "    vertex_tex_coord = tex_coord;\n"
+        "    vertex_tex_coord2 = vec2(1.0f - tex_coord.x, tex_coord.y);\n" // flip awesomeface
+        "}\n"
+    };
+
+    unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, vs_glsl, nullptr);
+    glCompileShader(vertex_shader);
+    int success;
+    char err_msg[1024] = {0};
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertex_shader, sizeof(err_msg), nullptr, err_msg);
+        cout << "Compile vertex shader failed: " << err_msg << endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Fragment Shader
+    const char *fs_glsl[] = {
+        "#version 460 core\n"
+        "in vec4 vertex_color;\n"
+        "in vec2 vertex_tex_coord;\n"
+        "in vec2 vertex_tex_coord2;\n" // flip awesomeface
+        "out vec4 frag_color;\n"
+        "uniform sampler2D tex1;\n"
+        "uniform sampler2D tex2;\n"
+        "void main() {\n"
+        "    frag_color = mix(texture(tex1, vertex_tex_coord), texture(tex2, vertex_tex_coord2), 0.2);\n"
+        "}\n"
+    };
+
+    unsigned int frag_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(frag_shader, 1, fs_glsl, nullptr);
+    glCompileShader(frag_shader);
+    glGetShaderiv(frag_shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(frag_shader, sizeof(err_msg), nullptr, err_msg);
+        cout << "Compile fragment shader failed: " << err_msg << endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Use shader
+    unsigned int shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, frag_shader);
+    glLinkProgram(shader_program);
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shader_program, sizeof(err_msg), nullptr, err_msg);
+        cout << "Link program failed: " << err_msg << endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glUseProgram(shader_program);
+
+    glUniform1i(glGetUniformLocation(shader_program, "tex1"), 0);
+    glUniform1i(glGetUniformLocation(shader_program, "tex2"), 1);
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, tex[0]);
+
+        glActiveTexture(GL_TEXTURE0 + 1);
+        glBindTexture(GL_TEXTURE_2D, tex[1]);
+
+        glBindVertexArray(vao);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
